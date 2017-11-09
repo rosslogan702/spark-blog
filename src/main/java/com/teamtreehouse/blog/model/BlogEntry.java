74e09b9f052e6abc93c10097deacb8e349a2f091
rosslogan702 <rosslogan702@gmail.com>
2017-11-09 20:22:15 +0000

    Initial commit

---
+++ src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -0,0 +1,11 @@
+package com.teamtreehouse.blog.model;
+
+/**
+ * Created by Ross on 09/11/2017.
+ */
+public class BlogEntry {
+    public boolean addComment(Comment comment) {
+        // Store these comments!
+        return false;
+    }
+}
---
+++ src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.blog.model;
+
+/**
+ * Created by Ross on 09/11/2017.
+ */
+public class Comment {
+}
