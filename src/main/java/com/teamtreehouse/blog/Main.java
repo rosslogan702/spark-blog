fb423d6a2be4ff8ae049b62bfd4750f5789d3351
rosslogan702 <rosslogan702@gmail.com>
2017-11-13 22:00:48 +0000

    Adding CSS to main page

--- src/main/java/com/teamtreehouse/blog/Main.java
+++ src/main/java/com/teamtreehouse/blog/Main.java
@@ -1,8 +1,27 @@
 package com.teamtreehouse.blog;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 /**
  * Created by Ross on 09/11/2017.
  */
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        staticFileLocation("/public.css");
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
