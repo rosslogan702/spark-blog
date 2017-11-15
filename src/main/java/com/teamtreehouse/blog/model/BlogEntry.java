2964e7c2096eb0a1e46102d6cce6a069d40c6637
rosslogan702 <rosslogan702@gmail.com>
2017-11-15 20:26:42 +0000

    Adding initial classes for modelling the blog posts and comments

--- src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,11 +1,68 @@
 package com.teamtreehouse.blog.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import com.github.slugify.Slugify;
+
 /**
  * Created by Ross on 09/11/2017.
  */
 public class BlogEntry {
+
+    private String title;
+    private String body;
+    private Set<Comment> comments;
+    //TODO: Add in the author?
+    private String slug;
+
+    public BlogEntry(String title, String body){
+        this.title = title;
+        this.body = body;
+        comments = new HashSet<>();
+        Slugify slugify = new Slugify();
+        slug = slugify.slugify(title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
+    public String getSlug(){
+        return slug;
+    }
+
+    public List<Comment> getComments(){
+        return new ArrayList<>(comments);
+    }
+
     public boolean addComment(Comment comment) {
-        // Store these comments!
-        return false;
+        return comments.add(comment);
+    }
+
+    //TODO: Placeholder in case we need to return the total number of comments for a blog post
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogEntry blogEntry = (BlogEntry) o;
+
+        if (!title.equals(blogEntry.title)) return false;
+        return body.equals(blogEntry.body);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + body.hashCode();
+        return result;
     }
 }
--- src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -4,4 +4,22 @@ package com.teamtreehouse.blog.model;
  * Created by Ross on 09/11/2017.
  */
 public class Comment {
+
+    private String name;
+    private String body;
+    //TODO: Possibly add in creation time that we will show
+    //This might be calculated from a library implicitly rather than box to add
+
+    public Comment(String name, String body){
+        this.name = name;
+        this.body = body;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getBody(){
+        return body;
+    }
 }
